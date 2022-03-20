70264b83e05715c0d17846a36ce35d8e83983594
Ben N <ben@Bens-MacBook-Pro.local>
2022-03-20 15:47:00 +0200

    updates

--- rate/urls.py
+++ rate/urls.py
@@ -1,12 +1,7 @@
 from django.urls import path
 from . import views
 
-from django.urls import include
-
 urlpatterns = [
-    path('', views.home, name="rmp-home"), 
-    # rmp-home
-    path('about/', views.about, name="rmp-about"), 
     path('list/', views.list, name="rmp-list"),
     path('view/', views.view, name="rmp-view"),
     path('avg/', views.avg, name="rmp-avg"),
@@ -14,11 +9,8 @@ urlpatterns = [
     path('login/', views.login, name="rmp-login"),
     path('logout/', views.logout_, name="rmp-logout"),
     path('rate/', views.rate, name="rmp-rate"),
-    # path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    # path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    # path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls'))
-
 ]
+
 from rest_framework.authtoken import views
 urlpatterns += [
     path('api-token-auth/', views.obtain_auth_token)
--- rate/views.py
+++ rate/views.py
@@ -1,9 +1,7 @@
 from random import randint
-from urllib import response
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from django.db.models import Sum, Avg
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
@@ -11,16 +9,10 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework import permissions
 
 from .models import Module, ModuleInstance, Professor, Rating, User
-from . serializers import ModuleInstanceSerializer, ProfessorSerializer, RatingSerializer, UserSerializer
+from . serializers import ModuleInstanceSerializer, RatingSerializer, UserSerializer
 import json
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate
 
-# from rate_my_prof.rate import serializers
-def home(request):
-    return render(request, 'rate/home.html')
-
-def about(request):
-    return render(request, 'rate/about.html')
 
 @api_view(['POST'])
 def register(request):
@@ -30,12 +22,10 @@ def register(request):
         if serializer.is_valid():
             try:
                 print (serializer)
-                # serializer.save(user = request.user)
                 serializer.save()
                 response = json.dumps([{ 'Success':'Registration Successful'}])
             except:
                 response = json.dumps([{'Error': 'Registration unsuccessful!'}])
-                # return JsonResponse(serializer.data, safe=False)
             return JsonResponse(response, safe=False)
         else:
             return JsonResponse(serializer.errors, safe=False)
@@ -48,34 +38,26 @@ def login(request):
     password = request.POST['password']
     user = authenticate(request, username=username, password=password)
     if user is not None:
-        # login(request, user)
-        # Redirect to a success page.
         response = json.dumps([{'Success':'Logged in'}])
-
     else:
-        # Return an 'invalid login' error message.
         response = json.dumps([{'Error':'Login failed'}])
-
     return JsonResponse(response, safe=False)
 
 @api_view(['POST'])
 def logout_(request):
     request.user.auth_token.delete()
-    # logout(request)
     response = json.dumps([{'Success':'You are logged out'}])
     return JsonResponse(response, safe=False)
 
 @api_view(['GET'])
 def list(request):
-    # output code
-
     if request.method == 'GET':
-        # try:
-        list = ModuleInstance.objects.all()
-        serializer = ModuleInstanceSerializer(list, many=True)
-        response = serializer.data
-        # except:
-            # response = json.dumps([{'Error':'No Records Found'}])
+        try:
+            list = ModuleInstance.objects.all()
+            serializer = ModuleInstanceSerializer(list, many=True)
+            response = serializer.data
+        except:
+            response = json.dumps([{'Error':'No Records Found'}])
         return JsonResponse(response, safe=False)
 
 @api_view(['GET'])
@@ -84,7 +66,6 @@ def view(request):
         try:
             response = []
             view = Professor.objects.all()
-            # for every professor
             for prof in view:
                 avg = Rating.objects.filter(professor=Professor.objects.get(code=prof.code).id).aggregate(Avg('rating'))
                 prof_rate = {"code": prof.code, "name": prof.name, "avg": avg.get('rating__avg')}
@@ -101,7 +82,7 @@ def avg(request):
     try:
         profCode = request.query_params.get('code')
         modCode = request.query_params.get('module')
-        modCode = Module.objects.get(code=modCode)
+        modCode = ModuleInstance.objects.get(code=modCode)
         professor =  Professor.objects.get(code=profCode)
         avg = Rating.objects.filter(professor=Professor.objects.get(code=profCode).id, module=modCode).aggregate(Avg('rating'))
         prof_rate = {"code": professor.code, "name": professor.name, "avg": avg.get('rating__avg')}
@@ -113,41 +94,32 @@ def avg(request):
 
 
 @api_view(['POST'])
-# @authentication_classes([SessionAuthentication, BasicAuthentication])
 @permission_classes([IsAuthenticated])
 def rate(request):
-    # if authenticated
-    # prevent web spam passing through - valdidation
-    # fix year **
-    # fix username
-    # prevent double rating
-
     if request.method =='POST':
         try:
+            data = request.data
+            year = request.data.get('year') #not exact year
+            year = year + "-01-01"
 
-                data = request.data
-                year = request.data.get('year') #not exact year
-                year = year + "-01-01"
+            semester = request.data.get('semester')
+            profCode = request.data.get('professor')
+            professor =  Professor.objects.get(code=profCode).pk
+            moduleCode = request.data.get('module')
+            module =  Module.objects.get(code=moduleCode).pk
+            rating = request.data.get('rating')
+            moduleInstance = ModuleInstance.objects.filter(module=module, year=year, semester = semester)[0]
+            user = User.objects.get(username=request.user.username)
+            if ((Rating.objects.filter(username=user, module=moduleInstance).count()) != 0):
+                response = json.dumps([{'Error':'Module rating previously submitted.'}])
+                return JsonResponse(response, safe=False)
 
-                semester = request.data.get('semester')
-                profCode = request.data.get('professor')
-                professor =  Professor.objects.get(code=profCode).pk
-                moduleCode = request.data.get('module')
-                module =  Module.objects.get(code=moduleCode).pk
-                rating = request.data.get('rating')
-                moduleInstance = ModuleInstance.objects.filter(module=module, year=year, semester = semester)[0]
-                user = User.objects.get(username=request.user.username)
-                if ((Rating.objects.filter(username=user, module=moduleInstance).count()) != 0):
-                    # Rating.objects.
-                    response = json.dumps([{'Error':'Module rating previously submitted.'}])
-                    return JsonResponse(response, safe=False)
- 
-                data = {
-                    "username":user.pk, 
-                    "professor":professor,
-                    "module":moduleInstance.pk,
-                    "rating":rating
-                }
+            data = {
+                "username":user.pk, 
+                "professor":professor,
+                "module":moduleInstance.pk,
+                "rating":rating
+            }
         except Exception as e:
             response = json.dumps([{'Error':'Invalid rating data'}])
             raise e
@@ -155,12 +127,10 @@ def rate(request):
         serializer = RatingSerializer(data=data)
         if serializer.is_valid(raise_exception=True):
             try:
-            # serializer.save(username = request.user)
                 serializer.save()
                 response = json.dumps([{ 'Success':'Rating added successfully!'}])
             except:
                 response = json.dumps([{'Error': 'Rating unsuccessful!'}])
-            # #    return JsonResponse(serializer.errors, safe=False)
             return JsonResponse(response, safe=False)
         else:
             return JsonResponse(serializer.errors, safe=False)
